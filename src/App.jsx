c074164b9ac0be468da4ab8ba7ba51a78165c8af
maydamv <moralesvieramayda@gmail.com>
2024-05-11 23:14:34 -0400

    add new global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import './App.css'
 import Navbar from './Navbar'
 import Home from './Home'
 
 function App() {
-
+  
   return (
     <>
+
     <Navbar />
-    <div className="content">
-        <Home></Home>
     
-    </div>
-   
-      
+    <div className="content">
+      <Home></Home>
+    </div> 
+
+
     </>
   )
+
 }
 
 export default App
