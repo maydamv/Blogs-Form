e8f1a8c1c75306ce557da6fdb31487088c020ad5
maydamv <moralesvieramayda@gmail.com>
2024-05-11 20:13:04 -0400

    add Navbar  component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <div className="content">
+        <h1>App Component</h1>
+    </div>
+   
+      
+    </>
+  )
+}
+
+export default App
