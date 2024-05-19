5a09cca8a367231bd962c14b3021a04a321f493b
maydamv <moralesvieramayda@gmail.com>
2024-05-19 13:49:47 -0400

    Add useState for Blogs

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react"
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        {title: 'My new wbsite', body: 'algooo', author: 'Mayda', id: 1},
+        {title: 'Welcome Party!', body: 'algooo', author: 'Carlos', id: 2},
+        {title: 'University UCLV', body: 'algooo', author: 'Bosco', id: 3}    
+    ]);
+ 
+
     return (  
         <div className="Home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Writen by {blog.author}</p>
+                </div>
+            ))}
         </div>   
     );
 }
